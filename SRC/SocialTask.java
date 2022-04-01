b421125834aea8c04e0d8326479584082bf0bb1e
Justin Jimenez <j_j243@txstate.edu>
2022-03-31 20:26:50 -0500

    Adding SocialTask and Social Factory for task type

---
+++ SRC/SocialTask.java
@@ -0,0 +1,42 @@
+//creating SocialTask that will implement Basictask to include the basic task variables
+public class SocialTask extends BasicTask{
+    private TaskType type = TaskType.SOCIAL;
+
+    //additional variables to be added when a social task is selected
+    private String meetingLocation;
+    private String specialOccasion;
+    private double attendingHours;
+
+    //creating getters and setters for the social task variables
+    public TaskType getType() {
+        return type;
+    }
+
+    public void setType(TaskType type) {
+        this.type = type;
+    }
+
+    public String getMeetingLocation() {
+        return meetingLocation;
+    }
+
+    public void setMeetingLocation(String meetingLocation) {
+        this.meetingLocation = meetingLocation;
+    }
+
+    public String getSpecialOccasion() {
+        return specialOccasion;
+    }
+
+    public void setSpecialOccasion(String specialOccasion) {
+        this.specialOccasion = specialOccasion;
+    }
+
+    public double getAttendingHours() {
+        return attendingHours;
+    }
+
+    public void setAttendingHours(double attendingHours) {
+        this.attendingHours = attendingHours;
+    }
+}
