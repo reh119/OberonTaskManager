a87cdf8aa90936db117e7e51db64d8c7387e29f6
Grant Smith <gcs37@txstate.edu>
2022-04-04 05:27:54 -0500

    Added the taskParser class and functionality
    taskParser is a class devoted to parsing tasks into string vectors to allow for the GUI to display.

---
+++ SRC/SocialTaskStringifier.java
@@ -0,0 +1,18 @@
+import java.time.format.DateTimeFormatter;
+import java.util.Vector;
+
+public class SocialTaskStringifier implements ITaskStringifier {
+	
+	public Vector<String> stringifyTask(TaskInterface task) {
+		Vector<String> stringVec = new Vector<String>();
+		SocialTask socialTask = (SocialTask) task;
+		stringVec.add(socialTask.getTitle());
+		stringVec.add(socialTask.getDate().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")));
+		stringVec.add("Social");
+		stringVec.add(Integer.toString(socialTask.getPriority(), 10));
+		stringVec.add(socialTask.getMeetingLocation());
+		stringVec.add(socialTask.getSpecialOccasion());
+		stringVec.add(Double.toString(socialTask.getAttendingHours()));
+		return stringVec;
+	}
+}
