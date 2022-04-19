791a763e36a12b11b8abbab74d9157808ff02239
Rafael Hernandez <reh119@txstate.edu>
2022-04-19 17:36:26 -0500

    remove task id, make fields not public

--- SRC/oberonguinetbeans/taskManager.java
+++ SRC/oberonguinetbeans/taskManager.java
@@ -3,12 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package oberonguinetbeans;
-import CoreCode.TaskController;
-import CoreCode.TaskType;
-
-import java.util.Vector;
-
-import CoreCode.TaskAttribute;
 
 /**
  *
@@ -19,8 +13,7 @@ public class taskManager extends javax.swing.JFrame {
     /**
      * Creates new form taskManager
      */
-    public taskManager(TaskController controller) {
-    	C = controller;
+    public taskManager() {
         initComponents();
     }
 
@@ -42,7 +35,7 @@ public class taskManager extends javax.swing.JFrame {
         searchType = new javax.swing.JComboBox<>();
         searchB = new javax.swing.JLabel();
         searchTask = new javax.swing.JTextField();
-        javax.swing.JButton searchButton = new javax.swing.JButton();
+        searchButton = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         addTaskLabel = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
@@ -53,11 +46,11 @@ public class taskManager extends javax.swing.JFrame {
         dateField = new javax.swing.JTextField();
         priorityType = new javax.swing.JComboBox<>();
         jLabel5 = new javax.swing.JLabel();
-        javax.swing.JButton addButton = new javax.swing.JButton();
+        addButton = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        javax.swing.JButton jButton3 = new javax.swing.JButton();
-        javax.swing.JButton deleteTask = new javax.swing.JButton();
+        editTask = new javax.swing.JButton();
+        deleteTask = new javax.swing.JButton();
 
         jTable3.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jTable3.setModel(new javax.swing.table.DefaultTableModel(
@@ -79,27 +72,19 @@ public class taskManager extends javax.swing.JFrame {
         taskTableView.setFont(new java.awt.Font("Hiragino Mincho ProN", 1, 10)); // NOI18N
         taskTableView.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
             },
             new String [] {
-                "TaskID", "Title", "Priority", "Type", "Date"
+                "Title", "Priority", "Type", "Date"
             }
         ));
         jScrollPane3.setViewportView(taskTableView);
 
         sortBy.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "ID", "Title", "Date" }));
-        sortBy.addPopupMenuListener(new javax.swing.event.PopupMenuListener() {
-            public void popupMenuCanceled(javax.swing.event.PopupMenuEvent evt) {
-            }
-            public void popupMenuWillBecomeInvisible(javax.swing.event.PopupMenuEvent evt) {
-                sortByPopupMenuWillBecomeInvisible(evt);
-            }
-            public void popupMenuWillBecomeVisible(javax.swing.event.PopupMenuEvent evt) {
-            }
-        });
+
         jLabel7.setText("Sort Tasks By");
 
         searchType.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Title", "Priority", "Date", "Type" }));
@@ -169,7 +154,7 @@ public class taskManager extends javax.swing.JFrame {
 
         jLabel6.setText("Type");
 
-        taskType.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "School", "Work", "Social", "Basic" }));
+        taskType.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "School ", "Work", "Social ", "Basic" }));
         taskType.addPopupMenuListener(new javax.swing.event.PopupMenuListener() {
             public void popupMenuCanceled(javax.swing.event.PopupMenuEvent evt) {
             }
@@ -256,7 +241,7 @@ public class taskManager extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Impact", 3, 36)); // NOI18N
         jLabel1.setText("Oberon Task Manager");
 
-        jButton3.setText("Edit Task");
+        editTask.setText("Edit Task");
 
         deleteTask.setText("Delete task");
         deleteTask.addActionListener(new java.awt.event.ActionListener() {
@@ -274,7 +259,7 @@ public class taskManager extends javax.swing.JFrame {
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 349, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(editTask, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(deleteTask, javax.swing.GroupLayout.PREFERRED_SIZE, 1, Short.MAX_VALUE))
                 .addContainerGap())
         );
@@ -282,7 +267,7 @@ public class taskManager extends javax.swing.JFrame {
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(47, 47, 47)
-                .addComponent(jButton3)
+                .addComponent(editTask)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(deleteTask)
@@ -322,7 +307,7 @@ public class taskManager extends javax.swing.JFrame {
 
     private void taskTypePopupMenuWillBecomeInvisible(javax.swing.event.PopupMenuEvent evt) {//GEN-FIRST:event_taskTypePopupMenuWillBecomeInvisible
         // TODO add your handling code here:
-        comboselect(); 
+        comboselect(); //
         
     }//GEN-LAST:event_taskTypePopupMenuWillBecomeInvisible
 
@@ -331,106 +316,84 @@ public class taskManager extends javax.swing.JFrame {
     }//GEN-LAST:event_taskTypeActionPerformed
 
     private void addButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addButtonActionPerformed
-    	Vector<String> addTaskVec = new Vector<String>();
-    	addTaskVec.add(titleField.getText());
-    	addTaskVec.add(dateField.getText());
-    	addTaskVec.add(priorityType.getItemAt(priorityType.getSelectedIndex()));
-    	switch (taskType.getItemAt(taskType.getSelectedIndex())) {
-    	case "Basic": 		C.addTask(TaskType.BASIC, addTaskVec);
-    						break;
-    	case "School":		addTaskVec.add(schoolF.getCourseName());
-    						addTaskVec.add(schoolF.getHomeworkName());
-    						addTaskVec.add(schoolF.getStudyHours());
-    						C.addTask(TaskType.SCHOOL, addTaskVec);
-    						break;
-    	case "Work":		addTaskVec.add(workF.getProjectName());
-							addTaskVec.add(workF.getWorkingWith());
-							addTaskVec.add(workF.getHoursWorked());
-							C.addTask(TaskType.WORK, addTaskVec);
-							break;
-    	case "Social":		addTaskVec.add(socialF.getMeetingLocation());
-							addTaskVec.add(socialF.getOccasion());
-							addTaskVec.add(socialF.getAttendingHours());
-							C.addTask(TaskType.SOCIAL, addTaskVec);
-							break;
-    	default: 			break;
-    	};
+        // TODO add your handling code here:
+        // add button
     }//GEN-LAST:event_addButtonActionPerformed
 
     private void searchButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchButtonActionPerformed
         // TODO add your handling code here:
-    	switch(searchType.getItemAt(searchType.getSelectedIndex())) {
-    	case "Title":		C.findTasks(TaskAttribute.TITLE, searchTask.getText());
-    						break;
-    	case "Date":		C.findTasks(TaskAttribute.DATE, searchTask.getText());
-    						break;
-    	case "Type":		C.findTasks(TaskAttribute.TYPE, searchTask.getText());
-    						break;
-    	case "Priority":	C.findTasks(TaskAttribute.PRIORITY, searchTask.getText());
-    						break;
-    	}
-    	updateTaskTable(C.getGroupTaskStrings());
+        // search Task Button
     }//GEN-LAST:event_searchButtonActionPerformed
-    
-    private void sortByPopupMenuWillBecomeInvisible(javax.swing.event.PopupMenuEvent evt)
-    {
-    	int dropdownSelection = sortBy.getSelectedIndex();
-    	if (dropdownSelection == 0){ // ID
-            
-        }
-        if (dropdownSelection == 1){ // Title
-           C.sortTasks(TaskAttribute.TITLE);
-        }
-        if (dropdownSelection == 2){ // Date
-            C.sortTasks(TaskAttribute.DATE);
-        }
-        updateTaskTable(C.getGroupTaskStrings());
-    }
 
     private void deleteTaskActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteTaskActionPerformed
-        C.selectTask(taskTableView.getSelectedRow());
-        C.removeTask();
+        // TODO add your handling code here:
+        // delete task button
     }//GEN-LAST:event_deleteTaskActionPerformed
-    
-    private void updateTaskTable(Vector<Vector<String>> stringVec) {
-    	taskTableView.setModel(new javax.swing.table.DefaultTableModel(
-                new Object [stringVec.size()][4],
-                new String [] {
-                    "TaskID", "Title", "Priority", "Type", "Date"
-                }));
-    	for (int i = 0; i < stringVec.size(); i++)
-    	{
-    		taskTableView.getModel().setValueAt(stringVec.get(i).get(0), i, 1);
-    		taskTableView.getModel().setValueAt(stringVec.get(i).get(3), i, 2);
-    		taskTableView.getModel().setValueAt(stringVec.get(i).get(2), i, 3);
-    		taskTableView.getModel().setValueAt(stringVec.get(i).get(1), i, 4);
-    	}
-    }
 
     public void comboselect(){ // selection when user selects type of tasks 
         int test = taskType.getSelectedIndex();
         
         if (test == 0){ // school frame
-            schoolF = new schoolFrame (); 
+            schoolFrame schoolF = new schoolFrame (); 
             schoolF.setVisible(true);
             schoolF.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         }
         if (test == 1){ // work frame 
-            workF = new workFrame (); 
+            workFrame workF = new workFrame (); 
             workF.setVisible(true);
             workF.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         }
         if (test == 2){ // Social 
-            socialF = new socialFrame (); // school frame 
+            socialFrame socialF = new socialFrame (); // school frame 
             socialF.setVisible(true);
             socialF.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         }
-        // if the user wants basic tasks, they just fill in default fields on taskManager.java frame
+        // if the user wants basic tasks, they just fill in default fields on taskManager.java fram
         
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(taskManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(taskManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(taskManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(taskManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new taskManager().setVisible(true);
+            }
+        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addButton;
     private javax.swing.JLabel addTaskLabel;
+    private javax.swing.JTextField dateField;
+    private javax.swing.JButton deleteTask;
+    private javax.swing.JButton editTask;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -442,18 +405,14 @@ public class taskManager extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTable jTable3;
+    private javax.swing.JComboBox<String> priorityType;
     private javax.swing.JLabel searchB;
+    private javax.swing.JButton searchButton;
+    private javax.swing.JTextField searchTask;
     private javax.swing.JComboBox<String> searchType;
+    private javax.swing.JComboBox<String> sortBy;
+    private javax.swing.JTable taskTableView;
     private javax.swing.JComboBox<String> taskType;
     private javax.swing.JTextField titleField;
-    private javax.swing.JTextField dateField;
-    private javax.swing.JComboBox<String> priorityType;
-    private TaskController C;
-    private javax.swing.JTable taskTableView;
-    private javax.swing.JTextField searchTask;
-    private javax.swing.JComboBox<String> sortBy;
-    private schoolFrame schoolF;
-    private workFrame workF;
-    socialFrame socialF;
     // End of variables declaration//GEN-END:variables
 }
