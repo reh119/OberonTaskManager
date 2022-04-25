11bee90bf42c4a83c43bd5b5a3fb5387cae63982
Grant Smith <gcs37@txstate.edu>
2022-04-25 13:09:48 -0500

    Attempting to work on new GUI2 but unable to due to inadequate names

--- SRC/CoreCode/GUI2TaskView.java
+++ SRC/CoreCode/GUI2TaskView.java
@@ -1,6 +1,7 @@
 package CoreCode;
 
 import my.gui2.GUI2;
+import my.user2;
 
 public class GUI2TaskView extends View {
 
@@ -13,7 +14,27 @@ public class GUI2TaskView extends View {
 
     public void GUIStart()
     {
-
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(user2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(user2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(user2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(user2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new user2().setVisible(true);
+            }
+        });
     }
 
     public void resetForGUIChange()
