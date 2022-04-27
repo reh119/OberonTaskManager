bcf0b22687f81ae3e088497792a2fcaa811f15a8
Grant Smith <gcs37@txstate.edu>
2022-04-27 08:44:37 -0500

    Cleaned up main to be more presentable

--- SRC/main.java
+++ SRC/main.java
@@ -6,7 +6,7 @@ import CoreCode.*;
 
 public class main {
     public static void main(String[] args) {
-    	// initializing
+         // initializing
          TaskModel modelM = new TaskModel();
          TaskController C = new TaskController();
          modelM.setController(C);
@@ -15,30 +15,5 @@ public class main {
          viewer.setController(C);
          C.setView(viewer);
          viewer.GUIStart();
-         for(boolean j = false; j == false;)
-         {
-         int selectedTask;
-         int selection;
-         System.out.print("SELECT TASK INDEX TO VIEW" + "\n");
-         Scanner obj = new Scanner(System.in);
-         selectedTask = Integer.parseInt(obj.nextLine());
-         C.selectTask(selectedTask);
-         Vector<String> testVecString = C.getCurrentTaskStrings();
-         for(int i = 0; i < testVecString.size(); i++)
-         {
-	         System.out.print(testVecString.get(i) + "\n");
-         }
-         System.out.print("ADD EXTRA GUI?" + "\n" + "YES: 1" + "\n" + "NO: 2" + "\n");
-         Scanner obj2 = new Scanner(System.in);
-         selection = Integer.parseInt(obj2.nextLine());
-         if (selection == 1)
-         {
-        	 GUI1TaskView viewer2 = new GUI1TaskView();
-        	 viewer2.setController(C);
-        	 viewer2.resetForGUIChange();
-        	 C.setView(viewer2);
-        	 viewer2.GUIStart();
-         }         
-         }
     }
 }
