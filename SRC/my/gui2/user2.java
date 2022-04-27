ec55c7ba8f30acc05f87a86d3fe256483f95b0d7
Grant Smith <gcs37@txstate.edu>
2022-04-27 01:41:52 -0500

    Trying to see if I can reliably merge between half finished and finished

--- SRC/my/gui2/user2.java
+++ SRC/my/gui2/user2.java
@@ -1,11 +1,11 @@
-
+package my.gui2;
 import java.util.Vector;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package my.gui2;
+
 
 /** 
 
