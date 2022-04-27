105c06091058c2ad7bc387567283a1f1a169a23f
Grant Smith <gcs37@txstate.edu>
2022-04-27 03:00:50 -0500

    Manual merging of two previous functs

--- SRC/my/gui2/socialTaskInfo.java
+++ SRC/my/gui2/socialTaskInfo.java
@@ -4,6 +4,8 @@
  */
 package my.gui2;
 
+import CoreCode.TaskController;
+
 /**
  *
  * @author oscarramirez
@@ -13,9 +15,12 @@ public class socialTaskInfo extends javax.swing.JFrame {
     /**
      * Creates new form socialTaskInfo
      */
-    public socialTaskInfo() {
+    public socialTaskInfo(TaskController C) {
         initComponents();
+        this.C = C;
     }
+
+    TaskController C;
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -261,37 +266,6 @@ public class socialTaskInfo extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(socialTaskInfo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(socialTaskInfo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(socialTaskInfo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(socialTaskInfo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new socialTaskInfo().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField attendingText;
